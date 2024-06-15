96c04646f9dba1577ed535161861d6687f244a89
Koopa123 <166151830+Koopa123@users.noreply.github.com>
2024-06-15 07:05:21 +0000

    prueba2

--- src/mainPAGINAPRINCIPAL.js
+++ src/mainPAGINAPRINCIPAL.js
@@ -25,7 +25,7 @@ const Main = () => {
                         <Carousel.Caption>
                             <h3>{it.name}</h3>
                             <p>{it.description}</p>
-                            <button className="btn btn-primary" >Ver detalle</button>
+                            <button className="btn btn-primary" >Ver detalle.</button>
                         </Carousel.Caption>
                     </Carousel.Item>);
                 })
